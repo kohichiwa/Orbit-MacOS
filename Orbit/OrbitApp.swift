53d3980268d35782967d39fe594e67f383dd7b63
kokha <kokhanenko.roman@gmail.com>
2026-07-17 13:40:45 +0300

    Initial Commit

---
+++ Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by Коханенко Роман on 17.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
